f8d0216349ddf1d25f8a61188cad93c71c76491a
nicolasances <nicolas.matteazzi@gmail.com>
2025-01-04 14:27:36 +0100

    Trying logs

--- dlg/test.py
+++ dlg/test.py
@@ -22,6 +22,8 @@ def test(request: Request, user_context: UserContext, exec_context: ExecutionCon
     html_content = scrape_blog(blog_url)
     
     exec_context.logger.log(exec_context.cid, f'Scraped content (first 50 chars) {html_content[0:50]}')
+    exec_context.logger.log(exec_context.cid, f'Scraped content (last 200 chars) {html_content[-200:]}')
+    exec_context.logger.log(exec_context.cid, f'Index of "The author denounces the wrong" - {html_content.find("The author denounces the wrong")}')
     
     # 2. Extract all the text
     text_content = BlobTextExtractor(html_content).get_text()
