64bc8d9ba44b9bba606063f8efedcf656f639abd
nicolasances <nicolas.matteazzi@gmail.com>
2025-06-21 16:12:58 +0200

    Right Topic name

--- dlg/scrape.py
+++ dlg/scrape.py
@@ -31,7 +31,7 @@ def scrape_and_store_blog(blog_url: str, topic_name: str, user: str, exec_contex
     kb_structure: StorageBlogStructure = KnowledgeBaseStorage(exec_context).store_blog_content(blog_content)
     
     # 5. Event on PubSub
-    event_publisher = TotoEventPublisher(kb_structure.topic_code, exec_context)
+    event_publisher = TotoEventPublisher('tometopics', exec_context)
     
     event_publisher.publishEvent(kb_structure.topic_code, 'topicScraped', f"The content of topic {kb_structure.topic_code} has been saved in the GCS Knowledge Base", {
         "topic_code": kb_structure.topic_code, 
