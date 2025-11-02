6798f9b5882160e4368f9de42f44fa7ef6ef5e46
nicolasances <nicolas.matteazzi@gmail.com>
2025-11-02 15:45:55 +0100

    Health endpoint for ALB on AWS

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ from evt.ontopic import on_topic_event
 app = Flask(__name__)
 CORS(app, origins=["*"])
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return {"api": "toto-ms-tome-scraper", "running": True}
+
+@app.route('/', methods=['GET'])
+def smoke_base():
+    return {"api": "toto-ms-tome-scraper", "running": True}
+
 @app.route('/tomescraper', methods=['GET'])
 def smoke():
     return {"api": "toto-ms-tome-scraper", "running": True}
