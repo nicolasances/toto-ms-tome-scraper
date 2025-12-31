d9543bf49225901bd93478dd4480cbda76c48671
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-31 14:30:30 +0100

    Toto MS 1.0.1

--- config/config.py
+++ config/config.py
@@ -4,14 +4,6 @@ from typing import Optional, Dict
 class TomeScraperConfig(TotoControllerConfig):
     """Custom configuration for the Toto Tome Scraper service."""
     
-    def get_api_name(self) -> str:
-        """Return the API name."""
-        return "toto-ms-tome-scraper"
-    
-    def get_expected_audience(self) -> str:
-        """Return the expected JWT audience."""
-        return "toto-ms-tome-scraper"
-    
     def get_mongo_secret_names(self) -> Optional[Dict[str, str]]:
         """Return MongoDB secret names if service uses MongoDB."""
         return None
