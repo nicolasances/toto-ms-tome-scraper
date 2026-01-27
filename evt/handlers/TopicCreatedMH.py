d227695160e735e4349fa695c47cfb5dd73ad0bb
nicolasances <nicolas.matteazzi@gmail.com>
2026-01-27 13:59:40 +0100

    Topic Created Message Handler

---
+++ evt/handlers/TopicCreatedMH.py
@@ -0,0 +1,34 @@
+
+from dlg.scrape import scrape_and_store_blog
+from totoms.evt.TotoMessage import TotoMessage
+from totoms.evt.TotoMessageBus import ProcessingResponse, ProcessingStatus
+from totoms.evt.TotoMessageHandler import TotoMessageHandler
+from totoms.model.ExecutionContext import ExecutionContext
+
+class TopicCreatedEventHandler(TotoMessageHandler): 
+    
+    def get_handled_message_type(self) -> str:
+        return "topicCreated"
+    
+    async def process_message(self, message: TotoMessage) -> ProcessingResponse:
+        # Implement the logic to handle the topic created event
+        self.logger.log(message.cid, f"Processing TopicCreated event: {message.msg}")
+        
+        blog_url = message.data.get('blogURL')
+        topic_name = message.data.get('name')
+        topic_id = message.id
+        user = message.data.get('user')
+        
+        exec_context = ExecutionContext(
+            logger=self.logger,
+            cid=message.cid, 
+            config=self.config, 
+            message_bus=self.message_bus,
+            environment=self.environment
+        )
+        
+        # Start the scraping process
+        await scrape_and_store_blog(blog_url, topic_name, topic_id, user, exec_context)
+        
+        # For demonstration, we just log the message and return success
+        return ProcessingResponse(status=ProcessingStatus.SUCCESS)
